00dcb2827fc4b93f7c894f595cc3f7ed93e338c6
bakerkai <bakerkai@urbandaleschools.com>
2022-01-26 12:58:51 -0600

    owwah

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -264,7 +264,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #######################################
 
 
-   elif player == 7:
+    elif player == 7:
         if getting_team_name:
             return 'Will F Team'
         else:
